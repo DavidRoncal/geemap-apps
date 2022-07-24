2d3b7377a6e008bf18307ac74de48ba2283d1d46
DavidRoncal <71480596+DavidRoncal@users.noreply.github.com>
2022-07-24 16:24:07 -0500

    Add files via upload

---
+++ apps/Raura_dem.py
@@ -0,0 +1,38 @@
+import streamlit as st
+import geemap
+import os
+import ee
+
+
+def app():
+    st.title("DEM Raura")
+
+    dem = ee.Image('USGS/SRTMGL1_003')
+
+    Map = geemap.Map()
+    Map.add_basemap('HYBRID')
+    Map = geemap.Map(center=[-10.53,-76.7519], zoom=9)
+    
+    # Set the region of interest by simply drawing a polygon on the map
+    region = Map.user_roi
+    if region is None:
+        region = ee.Geometry.Polygon(
+        [[[-77.25522258180023, -10.001142906420185],
+          [-77.25522258180023, -11.006411564927092],
+          [-76.21323802369476, -11.006411564927092],
+          [-76.21323802369476, -10.001142906420185]]])
+
+
+
+    # Set the date range
+
+    vis_params = {
+    "min": 4149.245450898364,
+    "max": 5257.294794667802
+    }
+
+    Map.addLayer(dem.clip(region), vis_params, 'STRM DEM', True, 1)
+    Map
+
+    # Add Earth Engine layers to Map
+    Map.to_streamlit(height=650)
