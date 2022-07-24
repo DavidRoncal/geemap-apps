1e86a23b7b66a1dd3f798f633eccb73606769026
DavidRoncal <71480596+DavidRoncal@users.noreply.github.com>
2022-07-24 16:24:42 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from multiapp import MultiApp
-from apps import Raura
+from apps import Raura, Raura_dem
 
 st.set_page_config(layout="wide")
 
@@ -9,7 +9,8 @@ apps = MultiApp()
 
 # Add all your application here
 
-apps.add_app("Raura Imagen Satelital", Raura.app)
+apps.add_app("Imagen Satelital", Raura.app)
+apps.add_app("Modelo Digital de Elevación", Raura_dem.app)
 
 # The main app
 apps.run()
