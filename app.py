3e2c44af04c1a9ac01a6fbc51c8ea67824260190
DavidRoncal <71480596+DavidRoncal@users.noreply.github.com>
2022-07-24 10:24:38 -0500

    Update app.py

--- app.py
+++ app.py
@@ -9,12 +9,7 @@ apps = MultiApp()
 
 # Add all your application here
 
-apps.add_app("Home", home.app)
-apps.add_app("Customize the default map", customize.app)
-apps.add_app("Change basemaps", basemaps.app)
-apps.add_app("Change opacity", opacity.app)
-apps.add_app("Search datasets", datasets.app)
-apps.add_app("NLCD Demo", nlcd_demo.app)
+apps.add_app("Raura Imagen Satelital", Raura.app)
 
 # The main app
 apps.run()
