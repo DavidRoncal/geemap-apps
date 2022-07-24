55b033c2ec65d82edb395ab718a3cd5791a0faab
DavidRoncal <71480596+DavidRoncal@users.noreply.github.com>
2022-07-24 18:47:05 -0500

    Update Raura.py

--- apps/Raura.py
+++ apps/Raura.py
@@ -12,7 +12,7 @@ def app():
     table = ee.FeatureCollection("users/davidroncal123/AEA_RAURA")
     
     Map = geemap.Map()
-    Map = geemap.Map.centerObject(table, 11)
+    Map = geemap.Map(center=[-10.53,-76.7519], zoom=11)
     Map.add_basemap(basemap)
     # Add Earth Engine dataset
     Raura_RGB = ee.Image('users/davidroncal123/Imagen_truecolor_geo')
